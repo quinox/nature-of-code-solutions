1d7b4ec587fb7ec5fda1e6b26de69c6cd86a5c9a
quinox <quinox@qtea.nl>
2017-08-19 10:36:46 +0200

    Add ESLint

--- solutions/exercise-1.02.js
+++ solutions/exercise-1.02.js
@@ -1,119 +1,121 @@
+/* exported setup */
+/* exported draw */
+
 var mousevect_location; // where the vector starts
 var default_vector_length = 20;
 
 function setup() {
-    createCanvas(500, 500, P2D);
+  createCanvas(500, 500, P2D);
 
-    mousevect_location = createVector(width / 2, height / 2);
+  mousevect_location = createVector(width / 2, height / 2);
 }
 
 function draw() {
-    background(255);
-    
-    var mouse_vector = createVector(mouseX, mouseY);
-    mouse_vector.sub(mousevect_location); // shift reference
-    mouse_vector.normalize();
+  background(255);
+  
+  var mouse_vector = createVector(mouseX, mouseY);
+  mouse_vector.sub(mousevect_location); // shift reference
+  mouse_vector.normalize();
 
-    draw_mouse_vector(mouse_vector.copy());
+  draw_mouse_vector(mouse_vector.copy());
 
-    vector_in_vector_out = [
-        ['add()', 'add'],
-        ['sub()', 'sub'],
-        ['cross() [results end up in the 3rd dimension]', 'cross'],
-    ];
+  let vector_in_vector_out = [
+    ['add()', 'add'],
+    ['sub()', 'sub'],
+    ['cross() [results end up in the 3rd dimension]', 'cross'],
+  ];
 
-    vector_in_scalar_out = [
-        ['dot()', 'dot'],
-        ['dist()', 'dist'],
-        ['angleBetween()', 'angleBetween'],
-    ];
+  //let vector_in_scalar_out = [
+  //  ['dot()', 'dot'],
+  //  ['dist()', 'dist'],
+  //  ['angleBetween()', 'angleBetween'],
+  //];
 
-    scalar_in_vector_out = [
-        ['mult()', 'mult'],
-        ['div()', 'div'],
-        ['rotate()', 'rotate'],
-    ];
+  //let scalar_in_vector_out = [
+  //  ['mult()', 'mult'],
+  //  ['div()', 'div'],
+  //  ['rotate()', 'rotate'],
+  //];
 
-    scalar_in_scalar_out = [
-    ];
+  //let scalar_in_scalar_out = [
+  //];
 
-    nothing_in_scalar_out = [
-        ['mag()', 'mag'],
-        ['magSq()', 'magSq'],
-        ['heading()', 'heading'],
-    ];
+  //let nothing_in_scalar_out = [
+  //  ['mag()', 'mag'],
+  //  ['magSq()', 'magSq'],
+  //  ['heading()', 'heading'],
+  //];
 
-    var offset = createVector(20, 20);
-    for (var i = 0; i < vector_in_vector_out.length; i++) {
-        var title = vector_in_vector_out[i][0];
-        var effect = vector_in_vector_out[i][1];
-        push();
-        draw_vector_effect(title, effect, offset.copy(), mouse_vector.copy());
-        pop();
-        offset.y += 80;
-    }
+  var offset = createVector(20, 20);
+  for (var i = 0; i < vector_in_vector_out.length; i++) {
+    var title = vector_in_vector_out[i][0];
+    var effect = vector_in_vector_out[i][1];
+    push();
+    draw_vector_effect(title, effect, offset.copy(), mouse_vector.copy());
+    pop();
+    offset.y += 80;
+  }
 }
 
 function draw_mouse_vector(mouse_vector) {
-    mouse_vector.mult(default_vector_length);
+  mouse_vector.mult(default_vector_length);
 
-    // Shadow line, full length
-    stroke(200);
-    line(mousevect_location.x, mousevect_location.y, mouseX, mouseY);
+  // Shadow line, full length
+  stroke(200);
+  line(mousevect_location.x, mousevect_location.y, mouseX, mouseY);
 
-    // Real line, normalized
-    mouse_vector.add(mousevect_location); // shift reference
-    strokeWeight(2);
-    stroke(0, 255, 0);
-    line(mousevect_location.x, mousevect_location.y, mouse_vector.x, mouse_vector.y);
-    strokeWeight(1);
+  // Real line, normalized
+  mouse_vector.add(mousevect_location); // shift reference
+  strokeWeight(2);
+  stroke(0, 255, 0);
+  line(mousevect_location.x, mousevect_location.y, mouse_vector.x, mouse_vector.y);
+  strokeWeight(1);
 
-    // Box it in
-    noFill();
-    stroke(0);
-    var boxsize = default_vector_length + 50;
-    var half_a_boxsize = boxsize / 2;
-    rect(
-        mousevect_location.x - half_a_boxsize, 
-        mousevect_location.y - half_a_boxsize, 
-        boxsize,
-        boxsize
-    );
-    noStroke();
-    fill(0);
-    text(
-        'Input unit vector', 
-        mousevect_location.x - half_a_boxsize, 
-        mousevect_location.y - half_a_boxsize - 10
-    );
+  // Box it in
+  noFill();
+  stroke(0);
+  var boxsize = default_vector_length + 50;
+  var half_a_boxsize = boxsize / 2;
+  rect(
+    mousevect_location.x - half_a_boxsize, 
+    mousevect_location.y - half_a_boxsize, 
+    boxsize,
+    boxsize
+  );
+  noStroke();
+  fill(0);
+  text(
+    'Input unit vector', 
+    mousevect_location.x - half_a_boxsize, 
+    mousevect_location.y - half_a_boxsize - 10
+  );
 }
 
 function draw_vector_effect(title, effect, offset, input_vector) {
-    // Math
-    var primary_vector = createVector(1, 1);
-    primary_vector.normalize();
-    var secondary_vector = p5.Vector[effect](primary_vector, input_vector.normalize());
-    
-    // Drawing
-    text(title, offset.x, offset.y);
-    text('x: ' + secondary_vector.x.toFixed(2) + ' y: ' + secondary_vector.y.toFixed(2) + ' z: ' + secondary_vector.z.toFixed(2), offset.x, offset.y + 15);
-    text('length: ' + secondary_vector.mag().toFixed(2) + ' units.', offset.x, offset.y + 30);
+  // Math
+  var primary_vector = createVector(1, 1);
+  primary_vector.normalize();
+  var secondary_vector = p5.Vector[effect](primary_vector, input_vector.normalize());
+  
+  // Drawing
+  text(title, offset.x, offset.y);
+  text('x: ' + secondary_vector.x.toFixed(2) + ' y: ' + secondary_vector.y.toFixed(2) + ' z: ' + secondary_vector.z.toFixed(2), offset.x, offset.y + 15);
+  text('length: ' + secondary_vector.mag().toFixed(2) + ' units.', offset.x, offset.y + 30);
 
-    // Scaling for UI
-    primary_vector.mult(default_vector_length);
-    secondary_vector.mult(default_vector_length);
+  // Scaling for UI
+  primary_vector.mult(default_vector_length);
+  secondary_vector.mult(default_vector_length);
 
-    offset.y += 40;
-    primary_vector_offset = primary_vector.copy();
-    primary_vector_offset.add(offset);
-    stroke(255, 0, 0);
-    line(offset.x, offset.y, primary_vector_offset.x, primary_vector_offset.y);
+  offset.y += 40;
+  let primary_vector_offset = p5.Vector.add(primary_vector, offset);
+  stroke(255, 0, 0);
+  line(offset.x, offset.y, primary_vector_offset.x, primary_vector_offset.y);
 
-    secondary_vector_offset = p5.Vector.add(secondary_vector, offset);
-    stroke(0, 255, 0);
-    line(primary_vector_offset.x, primary_vector_offset.y, secondary_vector_offset.x, secondary_vector_offset.y);
+  let secondary_vector_offset = p5.Vector.add(secondary_vector, offset);
+  stroke(0, 255, 0);
+  line(primary_vector_offset.x, primary_vector_offset.y, secondary_vector_offset.x, secondary_vector_offset.y);
 
-    stroke(0, 0, 255);
-    strokeWeight(2);
-    line(offset.x, offset.y, secondary_vector_offset.x, secondary_vector_offset.y);
+  stroke(0, 0, 255);
+  strokeWeight(2);
+  line(offset.x, offset.y, secondary_vector_offset.x, secondary_vector_offset.y);
 }
--- solutions/exercise-1.03.js
+++ solutions/exercise-1.03.js
@@ -1,3 +1,6 @@
+/* exported setup */
+/* exported draw */
+
 var tiny_location;
 var stationary_location;
 var velocity;
@@ -9,110 +12,110 @@ var border_hit_counter = 0;
 var border_max_hits = 30 * stationary_max_hits; // 'watchdog', in case we never hit the cube
 
 function setup() {
-    stationary_location = createVector(140, 140, 0);
-    createCanvas(500, 500, WEBGL);
-    background(0);
+  stationary_location = createVector(140, 140, 0);
+  createCanvas(500, 500, WEBGL);
+  background(0);
 
-    tiny_location = createVector(140 + stationary_radius + 4, 140 + stationary_radius, 100);
-    velocity = createVector(1.3,   2, 0.01); // We'll use the Z index for rotation, not position.
+  tiny_location = createVector(140 + stationary_radius + 4, 140 + stationary_radius, 100);
+  velocity = createVector(1.3,  2, 0.01); // We'll use the Z index for rotation, not position.
 }
 
 function draw() {
-    ortho(0.1, width, -height, 0.1, -500, 500);
+  ortho(0.1, width, -height, 0.1, -500, 500);
 
-    push();
-    draw_debug();
-    pop();
+  push();
+  draw_debug();
+  pop();
 
-    push();
-    draw_spinning_cube();
-    pop();
+  push();
+  draw_spinning_cube();
+  pop();
 
-    push();
-    draw_stationary_cube();
-    pop();
+  push();
+  draw_stationary_cube();
+  pop();
 }
 
 function draw_stationary_cube() {
-    if (stationary_hit_counter > stationary_max_hits || border_hit_counter > border_max_hits) {
-        stationary_hit_counter = 0;
-        border_hit_counter = 0;
-        stationary_location.x = random(0, width);
-        stationary_location.y = random(0, height);
-        stationary_radius = random(10, min(width, height) / 3);
-    }
+  if (stationary_hit_counter > stationary_max_hits || border_hit_counter > border_max_hits) {
+    stationary_hit_counter = 0;
+    border_hit_counter = 0;
+    stationary_location.x = random(0, width);
+    stationary_location.y = random(0, height);
+    stationary_radius = random(10, min(width, height) / 3);
+  }
 
-    translate(stationary_location.x, stationary_location.y, -20);
-    fill(
-        0 + (255 / stationary_max_hits * stationary_hit_counter),
-        255 - (255 / stationary_max_hits * stationary_hit_counter),
-        255 - (255 / stationary_max_hits * stationary_hit_counter)
-    );
-    box(stationary_radius * 2);
+  translate(stationary_location.x, stationary_location.y, -20);
+  fill(
+    0 + (255 / stationary_max_hits * stationary_hit_counter),
+    255 - (255 / stationary_max_hits * stationary_hit_counter),
+    255 - (255 / stationary_max_hits * stationary_hit_counter)
+  );
+  box(stationary_radius * 2);
 }
 
 function draw_debug() {
-    translate(0, 0, -20);
-    fill(255, 0, 0);
-    box(10);
+  translate(0, 0, -20);
+  fill(255, 0, 0);
+  box(10);
 
-    translate(width, height, -20);
-    fill(0, 255, 0);
-    box(10);
+  translate(width, height, -20);
+  fill(0, 255, 0);
+  box(10);
 
-    translate(width, 0.1, -20);
-    fill(0, 0, 255);
-    box(10);
+  translate(width, 0.1, -20);
+  fill(0, 0, 255);
+  box(10);
 
-    translate(0.1, height, -20);
-    fill(0, 255, 255);
-    box(10);
+  translate(0.1, height, -20);
+  fill(0, 255, 255);
+  box(10);
 }
 
 function draw_spinning_cube() {
-    tiny_location.add(velocity);
+  tiny_location.add(velocity);
 
-    // Don't hit the borders
-    if ((tiny_location.x + spinner_radius > width) || (tiny_location.x - spinner_radius < 0)) {
-        border_hit_counter += 1;
-        velocity.x = velocity.x * -1;
+  // Don't hit the borders
+  if ((tiny_location.x + spinner_radius > width) || (tiny_location.x - spinner_radius < 0)) {
+    border_hit_counter += 1;
+    velocity.x = velocity.x * -1;
+  }
+  if ((tiny_location.y + spinner_radius > height) || (tiny_location.y - spinner_radius < 0)) {
+    border_hit_counter += 1;
+    velocity.y = velocity.y * -1;
+  }
+
+  // Don't hit the stationary cube
+  if (
+    (tiny_location.x + spinner_radius > stationary_location.x - stationary_radius) &&
+    (tiny_location.x - spinner_radius < stationary_location.x + stationary_radius) &&
+    (tiny_location.y + spinner_radius > stationary_location.y - stationary_radius) &&
+    (tiny_location.y - spinner_radius < stationary_location.y + stationary_radius)
+  ) {
+    stationary_hit_counter += 1;
+    // Moving left made us crash into the stationary cube
+    if (velocity.x > 0 && tiny_location.x + spinner_radius - velocity.x <= stationary_location.x - stationary_radius) {
+      velocity.x = velocity.x * -1;
     }
-    if ((tiny_location.y + spinner_radius > height) || (tiny_location.y - spinner_radius < 0)) {
-        border_hit_counter += 1;
-        velocity.y = velocity.y * -1;
+    // Moving right made us crash into the stationary cube
+    if (velocity.x < 0 && tiny_location.x - spinner_radius - velocity.x >= stationary_location.x + stationary_radius) {
+      velocity.x = velocity.x * -1;
     }
-
-    // Don't hit the stationary cube
-    if (
-        (tiny_location.x + spinner_radius > stationary_location.x - stationary_radius) &&
-        (tiny_location.x - spinner_radius < stationary_location.x + stationary_radius) &&
-        (tiny_location.y + spinner_radius > stationary_location.y - stationary_radius) &&
-        (tiny_location.y - spinner_radius < stationary_location.y + stationary_radius)
-    ) {
-        stationary_hit_counter += 1;
-        // Moving left made us crash into the stationary cube
-        if (velocity.x > 0 && tiny_location.x + spinner_radius - velocity.x <= stationary_location.x - stationary_radius) {
-            velocity.x = velocity.x * -1;
-        }
-        // Moving right made us crash into the stationary cube
-        if (velocity.x < 0 && tiny_location.x - spinner_radius - velocity.x >= stationary_location.x + stationary_radius) {
-            velocity.x = velocity.x * -1;
-        }
-        // Moving down made us crash into the stationary cube
-        if (velocity.y > 0 && tiny_location.y + spinner_radius - velocity.y <= stationary_location.y - stationary_radius) {
-            velocity.y = velocity.y * -1;
-        }
-        // Moving up made us crash into the stationary cube
-        if (velocity.y < 0 && tiny_location.y - spinner_radius - velocity.y >= stationary_location.y + stationary_radius) {
-            velocity.y = velocity.y * -1;
-        }
+    // Moving down made us crash into the stationary cube
+    if (velocity.y > 0 && tiny_location.y + spinner_radius - velocity.y <= stationary_location.y - stationary_radius) {
+      velocity.y = velocity.y * -1;
     }
+    // Moving up made us crash into the stationary cube
+    if (velocity.y < 0 && tiny_location.y - spinner_radius - velocity.y >= stationary_location.y + stationary_radius) {
+      velocity.y = velocity.y * -1;
+    }
+  }
 
-    translate(tiny_location.x, tiny_location.y, -20);
-    rotateX(tiny_location.z * 3.001);
-    rotateY(tiny_location.z * 2.001);
+  translate(tiny_location.x, tiny_location.y, -20);
+  rotateX(tiny_location.z * 3.001);
+  rotateY(tiny_location.z * 2.001);
 
-    fill(255, 255, 0);
-    box(spinner_radius * 2);
-    return;
+  fill(255, 255, 0);
+  box(spinner_radius * 2);
+  return;
 }
